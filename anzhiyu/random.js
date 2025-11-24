995713edd2549997676dcf6ab2059f7f7ea9658e
eastsnowz <2188878913qq@gmail.com>
2025-11-24 21:51:00 +0800

    Site updated: 2025-11-24 21:51:00

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/11/23/1week/","2025/11/24/2week/","2025/11/13/hello-world/"];function toRandomPost(){
+var posts=["2025/11/24/2week/","2025/11/13/hello-world/","2025/11/23/1week/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
